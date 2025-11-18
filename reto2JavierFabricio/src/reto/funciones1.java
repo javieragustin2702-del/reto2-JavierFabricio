8c80b479c64c4856515982ae13efceb1335010ce
Fabricio Caceres <fabriciocaceres422@gmail.com>
2025-11-18 12:12:34 +0100

    hola

--- reto2JavierFabricio/src/reto/funciones1.java
+++ reto2JavierFabricio/src/reto/funciones1.java
@@ -1,5 +1,33 @@
 package reto;
 
 public class funciones1 {
+	
+	public static double millasAKilometros(double millas) {
+	    double kilometros = millas * 1.60934;
+	    return kilometros;
+	}
+ 
+	public static double kilometrosAmillas(double kilometros) {
+		double millas = kilometros * 0.621371;
+		return millas;
+	}
+	
+	public static double kilogramoAlibra (double kilogramo) {
+		double libra = kilogramo * 2.20462;
+		return libra;
+		
+	public static double libraAkilogramo (double libra) {
+		double kilogramo = libra * 0.453592;
+		return kilogramo;
+	}
+		
+		
+	}
+	
+	
+	
+	
+	
 
 }
+
